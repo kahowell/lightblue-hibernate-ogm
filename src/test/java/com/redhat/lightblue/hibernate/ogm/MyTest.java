ae8f0112ed5425d95a977d07044b9ceb8179eb6b
Dennis Crissman <dcrissma@redhat.com>
2015-05-29 10:33:27 -0400

    more work

--- src/test/java/com/redhat/lightblue/hibernate/ogm/MyTest.java
+++ src/test/java/com/redhat/lightblue/hibernate/ogm/MyTest.java
@@ -1,42 +1,57 @@
 package com.redhat.lightblue.hibernate.ogm;
 
+import static com.redhat.lightblue.util.test.AbstractJsonNodeTest.loadJsonNode;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-import org.hibernate.ogm.backendtck.associations.onetoone.Cavalier;
-import org.hibernate.ogm.backendtck.associations.onetoone.Horse;
 import org.junit.Test;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.redhat.lightblue.client.LightblueClient;
+import com.redhat.lightblue.hibernate.ogm.LightblueOgmJUnitRunner.LightblueTestMethods;
+import com.redhat.lightblue.hibernate.ogm.test.model.User;
 import com.redhat.lightblue.mongo.test.MongoServerExternalResource.InMemoryMongoServer;
 
 @InMemoryMongoServer
-public class MyTest extends AbstractLightblueOgmTestCase {
+public class MyTest extends AbstractLightblueOgmTestCase implements LightblueTestMethods {
+
+    private LightblueClient client;
+
+    @Override
+    public void setLightblueClient(LightblueClient client) {
+        this.client = client;
+    }
+
+    @Override
+    public JsonNode[] getMetadataJsonNodes() throws Exception {
+        return new JsonNode[]{
+                loadJsonNode("./metadata/user.json")
+        };
+    }
 
     @Override
     protected Class<?>[] getAnnotatedClasses() {
         return new Class<?>[]{
-                Horse.class,
-                Cavalier.class
+                User.class
         };
     }
 
     @Test
     public void testTheThing() {
+        User user = new User();
+        user.setFirstName("frank");
+        user.setLogin("fjones");
+        user.setNumberSites(2);
+        user.setUserId("1234");
+
         final Session session = openSession();
         Transaction transaction = session.beginTransaction();
-        Horse horse = new Horse("palefrenier");
-        horse.setName("Palefrenier");
-        Cavalier cavalier = new Cavalier("caroline");
-        cavalier.setName("Caroline");
-        cavalier.setHorse(horse);
-        session.persist(horse);
-        session.persist(cavalier);
+        session.persist(user);
         transaction.commit();
         session.clear();
         transaction = session.beginTransaction();
-        cavalier = (Cavalier) session.get(Cavalier.class, cavalier.getId());
-        horse = cavalier.getHorse();
-        session.delete(cavalier);
-        session.delete(horse);
+        User retrievedUser = (User) session.get(User.class, user.getUserId());
+        session.delete(retrievedUser);
         transaction.commit();
         session.close();
         checkCleanCache();
